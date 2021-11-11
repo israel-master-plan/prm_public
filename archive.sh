bad922d7d3b182247aeeaa3e251f5205205999d7
PRM <imp_prm@gmail.com>
2021-11-11 17:49:43 +0000

    released

--- archive.sh
+++
@@ -1,5 +0,0 @@
-output_dirname="distributions"
-
-[ ! -d "./$output_dirname" ] && mkdir "./$output_dirname"
-
-git archive --worktree-attributes --format=tar.gz HEAD > "$output_dirname/prm_v1.0.0.tar.gz"
